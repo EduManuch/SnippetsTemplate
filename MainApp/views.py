b1a740f63fcaf2568bceed54b68334d86b8051da
Eduard Manucharyan <madnus123@yandex.ru>
2023-05-14 17:57:23 +0300

    commit 1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,8 +10,32 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {'form': form,
+    'pagename': 'Добавление нового сниппета'}
     return render(request, 'pages/add_snippet.html', context)
+
+
+# Получаем данные формы --> Создаем сниппет
+def snippet_create(request):
+    if request.method == "POST":
+        # form_data = request.POST
+        # snippet = Snippet(
+        #     name=form_data['name'],
+        #     lang=form_data['lang'],
+        #     code=form_data['code'],
+        # )
+        # snippet.save()
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('snippets-list')
+
+
+def snippet_delete(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    snippet.delete()
+    return redirect('snippets-list')
 
 
 def snippets_page(request):
@@ -21,4 +46,10 @@ def snippets_page(request):
 
 
 def snippet_detail(request, snippet_id):
-    pass
+    snippet = Snippet.objects.get(id=snippet_id)
+    context = {
+        'pagename': 'Информация о сниппете',
+        'snippet': snippet,
+    }
+    return render(request, 'pages/snippet_detail.html', context)
+
