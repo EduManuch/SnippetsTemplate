6737beae052e9471b9ef59f13057a7e10d0f05f7
Eduard Manucharyan <madnus123@yandex.ru>
2023-05-21 12:00:37 +0300

    login/logout

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
+from django.contrib import auth
 
 
 def index_page(request):
@@ -52,3 +53,23 @@ def snippet_detail(request, snippet_id):
         'snippet': snippet,
     }
     return render(request, 'pages/snippet_detail.html', context)
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # print("username =", username)
+        # print("password =", password)
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            # Return error message
+            pass
+    return redirect('home')
+
+
+def logout_page(request):
+    auth.logout(request)
+    return redirect('home')
